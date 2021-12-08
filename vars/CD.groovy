2fdecc84c60ec487568affc539babd90af5a64aa
Shaurya Mishra <shaurya.mishra@gdn-commerce.com>
2021-12-08 17:44:45 +0530

    CD.groovy

--- vars/CD.groovy
+++ vars/CD.groovy
@@ -1,7 +1,7 @@
 def call(Map args){
         node{
            stage ('websiteCD - check minikube status') {
-                steps{
+
                     sh """
                     echo 'check status'
                     if minikube status | grep -q 'Running'; 
@@ -10,37 +10,37 @@ def call(Map args){
                     minikube start
                     fi
                     """
-                }
+                
            }
            stage ('websiteCD - delete existing pod') { 
-                steps{
+
                     sh """ 
                     if kubectl get pods | grep ${args.name}
                         then kubectl delete pod ${args.name}
                     fi
                     """ 
-                }
+                
 
            }
              stage ('websiteCD - run new pod') {
-                  steps{
+  
                     sh """ 
                     kubectl run ${args.name} --image=sadhorse22/${args.name}
                     """ 
-                  }	
+                  }
                }
               stage ('websiteCD - expose new pod') {
-                    steps {
+
                         sh """ 
                         if kubectl get svc | grep ${args.name}
                             then kubectl delete svc ${args.name}
                         fi
                         kubectl expose pod ${args.name} --type=NodePort --port=80 --target-port=8080
                         """ 
-                    } 
+                    }
                }
                 stage ('websiteCD - get website url') {
-                    steps{
+    
                         sh """ 
                         minikube ip
                         kubectl get service ${args.name}
