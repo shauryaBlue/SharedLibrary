f78cf7007bf64bad8811737b7894e26255621b3d
Shaurya Mishra <shaurya.mishra@gdn-commerce.com>
2021-12-08 17:50:24 +0530

    CD.groovy

--- vars/CI.groovy
+++ vars/CI.groovy
@@ -41,7 +41,7 @@ def call(Map args){
                   }
                   steps{
                     script{
-                        CD()
+                        CD(name: args.name)
                     }
                   }
                }
