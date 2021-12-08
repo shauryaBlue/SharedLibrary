b72771e8667aa30c4df3cb90b63c1eed067bbfaa
shauryaBlue <71072878+shauryaBlue@users.noreply.github.com>
2021-12-08 15:31:58 +0530

    Update CI.groovy

--- CI.groovy
+++ CI.groovy
@@ -1,1 +1,33 @@
-
+def call(String build_type, String name, String image_type){
+  pipeline {
+       agent any
+       stages {
+           stage ('websiteCI - Checkout') {
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '0a1990f2-e10e-49c1-a634-deb01976d44b', url: 'https://github.com/shauryaBlue/website.git']]]) 
+                sh """
+                pwd
+                """
+           }
+           stage ('websiteCI - Build war file') {
+                sh """ 
+                rm -f /.idea
+                rm -rf target/
+                mvn clean install
+                """ 
+           }
+           if(build_type == "Release"){
+             stage ('websiteCI - Build/Push docker image') { 	
+                    sh """ 
+                    docker build -t sadhorse22/${name} .
+                    docker push sadhorse22/${name}
+                    """ 
+               }
+              stage('Trigger Deployment') {
+                steps {
+                     build job: "websiteContinuousDelivery", wait: false, parameters: [string(name: 'name', value: String.valueOf(name))]
+                  }
+                }
+              }
+       }
+   }
+}
